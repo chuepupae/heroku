5e1321500927423f4efe6a587a7bbc0eae6bc64b
chuepupae <chanakanta60@email.nu.ac.th>
2021-01-16 02:03:35 +0700

    add test

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const router = require("./routes/user");
 const port = process.env.PORT || 5000;
 
-/* mongoose.connect('mongodb+srv://iboxconnect:ibox12345678@cluster0.2wpyq.mongodb.net/AppDB?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://iboxconnect:ibox12345678@cluster0.2wpyq.mongodb.net/AppDB?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
 });
- */
+
 
 const connection = mongoose.connection;
 connection.once("open", () => {
@@ -20,5 +21,6 @@ app.use(express.json());
 const userRoute = require("./routes/user");
 app.use("/user", userRoute);
 
-app.route("/").get((req, res) => res.json("Test API "));
+router.get('/test', (req, res) => { res.send('test') })
+app.route("/").get((req, res) => res.json("Welcome"));
 app.listen(port, () => console.log(`welcome your listinnig at port ${port}`));
